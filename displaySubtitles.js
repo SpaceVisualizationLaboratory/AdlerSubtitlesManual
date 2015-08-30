226282b43b2e7880ab9c09159f4f9fafc7543858
Jeffrey <subbaraojeffrey@gmail.com>
2015-08-30 13:54:30 -0400

    Added transition screens

--- displaySubtitles.js
+++ displaySubtitles.js
@@ -1,19 +1,36 @@
+j = 0;
+timestamps = [];
 function startSubtitles(){
-	text = document.getElementById("text");
-	originalHTML = text.innerHTML = initalMessage;
+	text = document.getElementById("text")
+	originalHTML = text.innerHTML
+	text.innerHTML = initalMessage;
 	i = 0;
-	waitTime = initalDelay;
-	j = 0;
+	startTime = new Date().getTime()
+	timestamps[0] = startTime + initalDelay*msPerTimeUnit
 	while (i < subtitles.length){
 		if (absoluteTiming){
-			waitTime = subtitles[i].time;
+			timestamps[i] = startTime+subtitles[i].time*msPerTimeUnit;
 		}
 		else{
 			if (i > 0){
-				waitTime += subtitles[i-1].time;
+				timestamps[i] = timestamps[i-1]+subtitles[i-1].time*msPerTimeUnit;
 			}
 		}
-		setTimeout(function(){text.innerHTML = tagSubtitles[0].concat(subtitles[j].text,tagSubtitles[1]);j++;},waitTime*msPerTimeUnit);
 		i++;
+	}
+	interalID = setInterval(checkTime,1);
+}
+function checkTime(){
+	var now = new Date();
+	if (now.getTime()>timestamps[j]){
+		text.innerHTML = subtitles[j].text;
+		j++;
+	}
+	else if (timestamps[j] - now.getTime() < transitionDuration * msPerTimeUnit){
+		text.innerHTML = transitionHTML;
+	}
+	if (j == timestamps.length)
+	{
+		clearInterval(intervalID);
 	}
 }
